a26186b218e87a3a617dc64f7bd042fd69a5e2f7
EC2 Default User <ec2-user@ip-172-31-23-81.us-east-2.compute.internal>
2018-07-17 04:40:55 +0000

    this stop at chapter 32

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ var express      = require("express"),
     app          = express(),
     mongoose     = require("mongoose"),
     bodyParser   = require("body-parser"),
-    Campground   = require("./models/campground");
+    Campground   = require("./models/campground"),
+    Comment      = require("./models/comment"),
+    seedDB       = require("./seed");
 
 
-
+seedDB();
 mongoose.connect("mongodb://localhost:27017/camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 // Campground.create({
@@ -36,7 +39,7 @@ app.get("/campgrounds",function(req,res){
             {
                 console.log(err);
             }else{
-                res.render("index",{campgrounds:allCampgrounds});
+                res.render("campgrounds/index",{campgrounds:allCampgrounds});
             }
         });
         // res.render("campgrounds",{campgrounds:campgrounds});
@@ -62,22 +65,60 @@ app.post("/campgrounds",function(req,res){
 
 //new -show form to create new campground
 app.get("/campgrounds/new",function(req,res){
-   res.render("new.ejs"); 
+   res.render("campgrounds/new"); 
 });
 
 //show - shows more info about one campground
 app.get("/campgrounds/:id",function(req,res){
     //find the campground with provided Id
-    Campground.findById(req.params.id, function(err,foundCampground ){
+    Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground ){
         if(err){
             console.log(err);
         }else{
+            console.log(foundCampground);
             //render show template with taht campground
-            res.render("show",{campground: foundCampground});
+            res.render("campgrounds/show",{campground: foundCampground});
         }
     }); 
+});
+//====================
+//Comment route
+//====================
+app.get("/campgrounds/:id/comments/new",function(req,res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new",{campground: campground});
+        }
+        
+    });
+});
+app.post("/campgrounds/:id/comments",function(req,res){
+    //lookup campground using ID
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+           
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect campground show page
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started!");
-})
+});
